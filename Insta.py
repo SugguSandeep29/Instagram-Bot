1428596753c9208dc3fe23085ed612f1e5fa9c24
SugguSandeep29 <68318127+SugguSandeep29@users.noreply.github.com>
2020-07-17 20:47:34 +0530

    Add files via upload

---
+++ Insta.py
@@ -0,0 +1,13 @@
+#instabot
+
+#open-cmd
+
+#
+
+
+from instabot import Bot
+	bot = Bot()
+		bot.login(username = "sandeepreddysuggu", password ="....."
+		#login credentials
+		bot.upload_photo("Ayyappa.jpg",caption = ".....")
+		#imagename & Caption
